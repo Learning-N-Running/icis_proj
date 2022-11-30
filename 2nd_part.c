84eb18ef7c22463dbe94d32a669f50363f28ae31
glorialeezero <110161434+glorialeezero@users.noreply.github.com>
2022-11-30 10:54:35 +0900

    메뉴로 돌아가기 다시 수정

--- 2nd_part.c
+++ 2nd_part.c
@@ -24,10 +24,13 @@ int second_part()
 
 	/**메뉴로 돌아가기**/
 	getchar();
-	printf("\nEnter를 누르시면 메뉴로 돌아갑니다.");
+	printf("\nEnter를 누르시면 메뉴로 돌아갑니다.\n그외의 값을 누르면 종료됩니다.\n");
 	scanf("%c", &go_to_menu);
-	if (go_to_menu == '\n')
+	if (go_to_menu == '\n') { //Enter를 누르면
 		printf("메뉴로 돌아갑니다.\n\n\n");
-	func_main();
+		func_main(); //메뉴로 돌아가는 함수 실행
+	}
+	else
+		printf("이용해 주셔서 감사합니다.\n안녕히계세요");
 	return 0;
 }
